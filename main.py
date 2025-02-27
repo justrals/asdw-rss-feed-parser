7611fa8a515b0b3a4e3d2d9ff1ce0d82cb2275c9
JustRals <87564009+JustRals@users.noreply.github.com>
2025-02-27 18:34:17 -0400

    added alerts guid check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pytz
 import os
+import json
+import pytz
 import feedparser
 import asyncio
 from telegram.ext import Application
@@ -14,35 +15,42 @@ CHANNEL_ID = os.getenv('CHANNEL_ID')
 
 application = Application.builder().token(TELEGRAM_TOKEN).build()
 
-LAST_ALERT_GUID_FILE = 'last_alert_guid.txt'
-
+SENT_GUIDS_FILE = 'sent_guids.json'
 halifax_tz = pytz.timezone('America/Halifax')
 
 
-def load_last_alert_guid():
-    if os.path.exists(LAST_ALERT_GUID_FILE):
-        with open(LAST_ALERT_GUID_FILE, 'r') as file:
-            return file.read().strip()
-    return None
+def load_sent_guids():
+    if os.path.exists(SENT_GUIDS_FILE):
+        with open(SENT_GUIDS_FILE, 'r') as file:
+            return json.load(file)
+    return []
 
 
-def save_last_alert_guid(guid):
-    with open(LAST_ALERT_GUID_FILE, 'w') as file:
-        file.write(guid)
+def save_sent_guids(guids):
+    with open(SENT_GUIDS_FILE, 'w') as file:
+        json.dump(guids, file)
+
+
+def add_sent_guid(guid):
+    sent_guids = load_sent_guids()
+    if guid not in sent_guids:
+        sent_guids.append(guid)
+        save_sent_guids(sent_guids)
+
+
+def extract_guid_number(guid_url):
+    return guid_url.split('p=')[1]
 
 
 def convert_time_zone(utc_time_str):
     utc_time = datetime.strptime(utc_time_str, "%a, %d %b %Y %H:%M:%S +0000")
     utc_time = pytz.utc.localize(utc_time)
-
     halifax_time = utc_time.astimezone(halifax_tz)
-
     return halifax_time.strftime('%Y-%m-%d %H:%M:%S')
 
 
 async def send_alert_to_telegram(alert_message, alert_date, alert_link):
     halifax_date = convert_time_zone(alert_date)
-
     message = f"*{alert_message}*\n\n_{halifax_date}_\n\n[Read More]({alert_link})"
     try:
         await application.bot.send_message(chat_id=CHANNEL_ID, text=message, parse_mode='Markdown')
@@ -50,22 +58,25 @@ async def send_alert_to_telegram(alert_message, alert_date, alert_link):
     except TimedOut:
         print("Request timed out. Retrying...")
         await application.bot.send_message(chat_id=CHANNEL_ID, text=message, parse_mode='Markdown')
+    except Exception as e:
+        print(f"Failed to send message: {e}")
 
 
 async def scan_feed():
-    last_alert_guid = load_last_alert_guid()
+    sent_guids = load_sent_guids()
     feed = feedparser.parse('https://asdw.nbed.ca/alerts/feed/')
 
-    for entry in feed.entries:
+    for entry in reversed(feed.entries):
         alert_message = entry.title
         alert_date = entry.published
         alert_link = entry.link
         alert_guid = entry.guid
 
-        if alert_guid != last_alert_guid:
+        guid_number = extract_guid_number(alert_guid)
+
+        if guid_number not in sent_guids:
             await send_alert_to_telegram(alert_message, alert_date, alert_link)
-            save_last_alert_guid(alert_guid)
-            last_alert_guid = alert_guid
+            add_sent_guid(guid_number)
 
 
 async def main():
